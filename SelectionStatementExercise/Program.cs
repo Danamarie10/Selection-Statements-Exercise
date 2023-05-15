343d4e297a063d6c972d101fd470fbd2d44a2ab2
Danamarie10 <dana.patrick10@outlook.com>
2023-05-15 14:41:09 -0400

    Finished Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //exercise 1
+            var favNum = 16;
+            var r = new Random();
+            var favNumber = r.Next(1, 50);
+
+            Console.WriteLine("Try to guess my favorite number between 1 and 50");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNum)
+            {
+                Console.WriteLine("Number is too low");
+            }
+            else if (userInput > favNum)
+            {
+                Console.WriteLine("Number is too High");
+            }
+            else
+            {
+                Console.WriteLine("That is Correct!");
+            }
+            
         }
     }
 }
